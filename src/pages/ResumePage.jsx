cb6da5ce8ae6ea6b6fe07335639eec69d2c4f043
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-02-08 07:51:54 -0500

    fixed links and mobile styling on portfolio page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 // Bringing in the required component from 'react-router-dom' for linking between pages
 import { Link } from 'react-router-dom';
-import Profile from '../components/UI/ProfileSections/ProfileTeaser';
-import ListItem from '../components/UI/ListItem';
+
 import Banner from '../components/Banner/index'
 
 import API from '../utils/API';
