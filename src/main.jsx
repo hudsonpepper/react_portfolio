91f3c966bc88dcecb96c848f1df08da508df0981
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-02-07 23:02:36 -0500

    added blank tailwindUI template to contact

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import "./index.css";
+// import "./index.css";
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 import App from "./App";
